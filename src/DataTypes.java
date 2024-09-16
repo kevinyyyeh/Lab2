795ad3827e4a2ca172ac83d679656112201ea0ce
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-09-16 17:52:00 -0400

    hi

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -4,7 +4,7 @@ public class DataTypes {
     // fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (long x : numbers) {
             s += x;
